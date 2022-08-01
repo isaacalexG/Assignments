0b77093f6ae94c264859b268e1d4482b35de8c05
isaacalexG <109628839+isaacalexG@users.noreply.github.com>
2022-08-01 17:21:30 -0500

    Add files via upload

---
+++ Day 10/ServletDemo/src/main/java/com/usk/demo/DemoServlet.java
@@ -0,0 +1,22 @@
+package com.usk.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Welcome to servlets!<h1>");
+	}
+
+}
